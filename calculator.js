770cd767fc18032c5bad3f9092c9e4fd40dfe345
Winnie <winnie.srisok@gmail.com>
2023-06-16 19:10:06 +0700

    Seperate keys pressed and adjust calculation

--- calculator.js
+++ calculator.js
@@ -3,74 +3,110 @@ const equalBtn = document.querySelector('.equal');
 const calBtn = document.querySelectorAll('#wrapper button');
 
 let firstValue = '';
-let secondValue = ''; 
+let secondValue = '';
 let operatorValue = '';
-let returnValue = '';
+let sum = '';
 
-const patternOperator = /\+|\-|x|\÷/g;
+const operators = ['+','-','x','÷','='];
+const numValues = ['0','1','2','3','4','5','6','7','8','9'];
 
 //make keys clickable and add entered value to output
 calBtn.forEach(item => {
-    // console.log(item.textContent)
     item.addEventListener('click', function() {
-        // console.log(item.textContent);
+
+        //reset after every calculation
+        if(sum) {
+            clearCalculator('')
+        }
         
-        //clear output on new calculation
-        if(returnValue || item.textContent === 'C') {
-            clearCalculator()
+        //reset when AC is clicked
+        if(item.textContent === 'AC') {
+            clearCalculator('0')
         }
 
-        //replace existing output value
-        if(outputDiv.textContent === '0') {
-            return outputDiv.textContent = item.textContent;
-        }else if(item.textContent === '=') {
-            //call function operate
-
-            const findOperator = outputDiv.textContent
-                .split('')
-                .find(item => patternOperator.test(item));
-
-            const findValues = outputDiv.textContent
-                .split(findOperator)
-
-            firstValue = findValues[0];
-            secondValue = findValues[1];
-            operatorValue = findOperator;
-
-            if(firstValue && secondValue && operatorValue) {
-                // console.log(firstValue)
-                // console.log(operatorValue)
-                // console.log(secondValue)
-                returnValue = operate(operatorValue, firstValue, secondValue);
-                return outputDiv.textContent = returnValue;
+        //operator is entered
+        if(operators.filter(operator => operator === item.textContent).length > 0) {
+            if(item.textContent !== '=') {
+                
+                if(outputDiv.textContent.length > 0) {
+                    //save firstvalue
+                    firstValue = outputDiv.textContent;
+                    //save operatorvalue
+                    operatorValue = item.textContent;
+                }else {
+                    //do nothing if output is empty
+                    clearCalculator('0')
+                    return
+                }
+                console.log('first: ' + firstValue)
+                console.log('oper: ' + operatorValue)
             }else {
-                return 0
+                if(outputDiv.textContent.length > 0) {
+                    //save secondvalue
+                    secondValue = outputDiv.textContent;
+                    console.log('sec: ' + secondValue)
+                }else {
+                    //do nothing if output is empty
+                    clearCalculator('0')
+                    return
+                }
             }
-        }else{
-            //keep adding values
-            return outputDiv.textContent += item.textContent;
+            
+            //start calculation if all variables have values
+            if(firstValue && secondValue && operatorValue) {
+                sum = operate(operatorValue, firstValue, secondValue);
+                outputDiv.textContent = sum;
+            }
         }
+        //number is pressed
+        if(numValues.filter(num => num === item.textContent).length > 0) {
+            //firstvalue
+            if(!firstValue) {
+                //replace 0 with number pressed
+                if(outputDiv.textContent === '0') {
+                    outputDiv.textContent = item.textContent;
+                }else {
+                    //continue add numbers pressed
+                    outputDiv.textContent += item.textContent;
+                }
+            }else {
+                //first number of secondvalue
+                if(!secondValue) {
+                    //add numbers pressed
+                    outputDiv.textContent = item.textContent;
+                    secondValue = item.textContent;
+                }else {
+                    //continue add numbers to secondvalue
+                    if(outputDiv.textContent === '0' && item.textContent === '0') {
+                        return
+                    }else {
+                        outputDiv.textContent += item.textContent;
+                    }
+                }
+            }       
+        }   
     });
 });
 
-function clearCalculator() {
-    outputDiv.textContent = '0';
+
+//reset calculator
+function clearCalculator(initialValue) {
+    if(outputDiv.textContent !== initialValue) {
+        outputDiv.textContent = initialValue;
+    }
     firstValue = '';
     secondValue = '';
     operatorValue = '';
-    returnValue = '';
+    sum = '';
 }
 
+//return sum depending on operator
 function operate(oper, num1, num2) {
-    //convert operator to string
-    // oper = oper.toString();
-
     //convert number values to numbers
     num1 = Number(num1);
     num2 = Number(num2);
 
     if(oper === '+') {
-        console.log(add(num1, num2))
         return add(num1, num2);
     }else if(oper === '-') {
         return substract(num1, num2);
@@ -84,6 +120,7 @@ function operate(oper, num1, num2) {
     }
 }
 
+//mutiple calculation functions
 function add(num1, num2) {
     return num1 + num2;
 };
